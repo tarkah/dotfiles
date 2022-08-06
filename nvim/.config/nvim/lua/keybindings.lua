c9e149faea318ecc05dbe51544c5f19c7a77ab52
Cory Forsstrom <cforsstrom18@gmail.com>
2022-08-06 15:58:24 -0700

    Migrate plugin configs to individual folders

--- nvim/.config/nvim/lua/keybindings.lua
+++ nvim/.config/nvim/lua/keybindings.lua
@@ -42,26 +42,26 @@ map('n', 'G', 'Gzz', {
 -- whichkey
 local wk = require("which-key")
 wk.register({
-   h = {"<cmd>nohlsearch<cr>", "clear search"},
-   e = {"<cmd>NvimTreeToggle<CR>", "explorer"},
-   w = { "<cmd>w!<CR>", "save" },
-   l = {
+    h = { "<cmd>nohlsearch<cr>", "clear search" },
+    e = { "<cmd>NvimTreeToggle<CR>", "explorer" },
+    w = { "<cmd>w!<CR>", "save" },
+    l = {
         name = "lsp",
-        f = {"<cmd>lua vim.lsp.buf.formatting()<cr>", "format"},
-        r = {"<cmd>lua require'telescope.builtin'.lsp_references{}<CR>", "references"},
-        d = {"<cmd>lua vim.lsp.buf.definition()<CR>", "definition"},
-        R = {"<cmd>lua vim.lsp.buf.rename()<CR>", "rename"},
-        s = {"<cmd>lua require'telescope.builtin'.lsp_document_symbols{}<CR>", "go to symbol"},
-        e = {"<cmd>lua require'telescope.builtin'.diagnostics{}<CR>", "errors"},
-        n = {"<cmd>lua vim.diagnostic.goto_next()<CR>", "next problem"},
-        p = {"<cmd>lua vim.diagnostic.goto_prev()<CR>", "prev problem"},
-        a = {"<cmd>lua vim.lsp.buf.code_action()<CR>", "code action"},
-        u = {"<cmd>lua require'rust-tools.parent_module'.parent_module()<CR>", "up one module"}
+        f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "format" },
+        r = { "<cmd>lua require'telescope.builtin'.lsp_references{}<CR>", "references" },
+        d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "definition" },
+        R = { "<cmd>lua vim.lsp.buf.rename()<CR>", "rename" },
+        s = { "<cmd>lua require'telescope.builtin'.lsp_document_symbols{}<CR>", "go to symbol" },
+        e = { "<cmd>lua require'telescope.builtin'.diagnostics{}<CR>", "errors" },
+        n = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "next problem" },
+        p = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "prev problem" },
+        a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "code action" },
+        u = { "<cmd>lua require'rust-tools.parent_module'.parent_module()<CR>", "up one module" }
     },
     c = {
         name = "+comment",
-        c = {"<Plug>kommentary_line_default", "line"},
-        m = {"<Plug>kommentary_motion_default", "motion"},
+        c = { "<Plug>kommentary_line_default", "line" },
+        m = { "<Plug>kommentary_motion_default", "motion" },
         v = {
             "<Plug>kommentary_visual_default",
             "section",
@@ -70,38 +70,38 @@ wk.register({
     },
     b = {
         name = "+buffers",
-        b = {"<cmd>Telescope buffers<cr>", "all buffers"},
-        d = {"<cmd>bd<CR>", "close Buffer"}
+        b = { "<cmd>Telescope buffers<cr>", "all buffers" },
+        d = { "<cmd>bd<CR>", "close Buffer" }
     },
     f = {
         name = "+file",
-        f = {"<cmd>Telescope find_files<cr>", "find files"},
-        j = {"<cmd>Telescope grep_string<cr>", "find word"},
-        g = {"<cmd>Telescope live_grep<cr>", "grep"},
-        s = {"<cmd>Telescope current_buffer_fuzzy_find<cr>", "search in file"},
+        f = { "<cmd>Telescope find_files<cr>", "find files" },
+        j = { "<cmd>Telescope grep_string<cr>", "find word" },
+        g = { "<cmd>Telescope live_grep<cr>", "grep" },
+        s = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "search in file" },
     },
     g = {
         name = "git",
-        s = {"<cmd>lua require'telescope.builtin'.git_status{}<CR>", "status"},
-        c = {"<cmd>lua require'telescope.builtin'.git_commits{}<CR>", "commits"},
-        d = {"<cmd>DiffviewOpen -uno<cr>", "diffsplit"}
+        s = { "<cmd>lua require'telescope.builtin'.git_status{}<CR>", "status" },
+        c = { "<cmd>lua require'telescope.builtin'.git_commits{}<CR>", "commits" },
+        d = { "<cmd>DiffviewOpen -uno<cr>", "diffsplit" }
     },
     p = {
         name = "packer",
-        s = {"<cmd>PackerSync<cr>", "sync"},
-        i = {"<cmd>PackerInstall<cr>", "install"},
-        c = {"<cmd>PackerCompile<cr>", "compile"},
-        l = {"<cmd>PackerClean<cr>", "clean"}
+        s = { "<cmd>PackerSync<cr>", "sync" },
+        i = { "<cmd>PackerInstall<cr>", "install" },
+        c = { "<cmd>PackerCompile<cr>", "compile" },
+        l = { "<cmd>PackerClean<cr>", "clean" }
     },
     s = {
         name = "session",
-        l = {"<cmd>SessionManager load_last_session<cr>", "last"},
-        s = {"<cmd>SessionManager load_session<cr>", "select"},
+        l = { "<cmd>SessionManager load_last_session<cr>", "last" },
+        s = { "<cmd>SessionManager load_session<cr>", "select" },
     },
     v = {
         name = "vim",
-        r = {":so $MYVIMRC<cr>", "reload init.lua"},
-        e = {":e $MYVIMRC<cr>", "edit init.lua"},
+        r = { ":so $MYVIMRC<cr>", "reload init.lua" },
+        e = { ":e $MYVIMRC<cr>", "edit init.lua" },
     }
 }, {
     prefix = "<leader>"
